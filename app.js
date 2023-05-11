802f4b04f34ae90311838ab7a18c3e161a62f6ca
cgmarko <marko.martinovic@icloud.com>
2023-05-11 15:52:59 +0200

    CORS changes

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const express = require("express");
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const app = express();
-var cors = require('cors')
+
 
 app.use(express.json());
-app.use(cors())
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // Logic goes here
 
 // importing user context
